352971684b4978ab1b0963b4f43f3e7bedfb0dd5
Luca Sammarco <luca.sammarco7@gmail.com>
2024-09-06 11:27:48 +0200

    added --auth scaffolding and bootstrap

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('/pages/welcome');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
