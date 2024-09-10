619f299712117c2a46a794410dfd7d7c1e1c715b
DanieleTrippodo <trippododaniele@gmail.com>
2024-09-10 12:01:05 +0200

    added pop-up on delete apartment

--- resources/views/user/apartment/index.blade.php
+++ resources/views/user/apartment/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.admin')
 
-@section('content')
+@section('content'){{-- test2 --}}
 
+<body>
 <div class="container">
     <div class="row">
         @foreach ($apartments as $apartment)
@@ -16,31 +17,18 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $apartment->title }}</h5>
                     </div>
-<div class="container">
-    <div class="row">
-        @foreach ($apartments as $apartment)
-            <div class="col-md-4">
-                <div class="card mb-3" style="width: 80%;">
-                    @if ($apartment->images) {{-- Se è disponibile la foto dell'appartamento --}}
-                        <img src="{{ asset('storage/' . $apartment->images) }}" class="card-img-top" alt="Missing">
-                    @else
-                        <img src="..." class="card-img-top" alt="Placeholder_Image"> {{-- Inserire immagine placeholder --}}
-                    @endif
-
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $apartment->title }}</h5>
-                    </div>
 
                     <div class="card-footer">
                         <div class="btn-group" role="group"> {{-- Azioni per appartamento --}}
                             <a href="{{ route('user.apartments.show', $apartment->id) }}" class="btn btn-primary me-1">Visualizza</a>
                             <a href="{{ route('user.apartments.edit', $apartment->id) }}" class="btn btn-primary me-1">Modifica</a>
 
-                            <form action="{{ route('user.apartments.destroy', $apartment->id) }}" method="POST" style="display:inline;">
+                            <form id="delete-form-{{ $apartment->id }}" action="{{ route('user.apartments.destroy', $apartment->id) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
+                                <button type="button" class="btn btn-danger" onclick="confirmDelete({{ $apartment->id }})">Elimina</button>
                             </form>
+
 
                         </div>
                     </div>
@@ -51,3 +39,27 @@
 </div>
 
 @endsection
+{{-- SweetAlert2 CDN --}}
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+
+
+<script>
+    function confirmDelete(apartmentId) {
+        Swal.fire({
+            title: 'Sei sicuro di voler eliminare questo appartamento?',
+            text: "Questa azione non può essere annullata!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sì, elimina!',
+            cancelButtonText: 'Annulla'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Invia il form di cancellazione
+                document.getElementById('delete-form-' + apartmentId).submit();
+            }
+        });
+    }
+</script>
