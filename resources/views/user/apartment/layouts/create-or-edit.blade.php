15b1e8300a20f69b0073c01737c021cf63a56e00
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-09-10 18:37:42 +0200

    creato layout create/edit unico

--- resources/views/user/apartment/layouts/create-or-edit.blade.php
+++ resources/views/user/apartment/layouts/create-or-edit.blade.php
@@ -1,103 +1,102 @@
 @extends('layouts.admin')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
-        {{-- Metodo per scrivere tutti gli errori dei vari campi in una sola finestra --}}
-        {{-- @if ($errors->any())
+
+        @if ($errors->any())
             <div class="col-8">
-                <ul>
+                <ul class="alert alert-danger">
                     @foreach ($errors->all() as $error)
-                        <li class="alert alert-danger">
-                            {{ $error }}
-                        </li>
+                        <li>{{ $error }}</li>
                     @endforeach
                 </ul>
             </div>
-        @endif --}}
-        <div class="col-8">
-            <form action="@yield('form-action')" method="POST" enctype="multipart/form-data">
-                @yield('form-method')
+        @endif
+
+        <div class="col-md-8">
+            <form action="{{ route('user.apartments.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-3">
-                    <h2>
-                        @yield('page-title')
-                    </h2>
+                    <label for="title" class="form-label">Titolo</label>
+                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="title">Title:</label>
-                    <input type="text" name="title" id="title" class="form-control mb-2" value="{{ old('title', $project->title) }}">
-                    @error("title")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                    <label for="rooms_num" class="form-label">Numero di Stanze</label>
+                    <input type="number" class="form-control" id="rooms_num" name="rooms_num" value="{{ old('rooms_num') }}" required>
                 </div>
+
                 <div class="mb-3">
-                    <label for="type_id">Type:</label>
-                    <select class="form-select" name="type_id">
-                        @foreach ( $types as $type )
-                            <option value="{{ $type->id }}" {{ $type->id == old('type_id', $project->type_id) ? 'selected' : ''}}> {{ $type->name }} </option>
-                        @endforeach
-                    </select>
-                    @error("type_id")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                    <label for="beds_num" class="form-label">Numero di Letti</label>
+                    <input type="number" class="form-control" id="beds_num" name="beds_num" value="{{ old('beds_num') }}" required>
                 </div>
+
                 <div class="mb-3">
-                    <label for="tags">Tags:</label>
-                    <div class="btn-group d-flex flex-wrap" role="group" aria-label="Basic checkbox toggle button group">
-                        @foreach ( $tags as $tag )
-                            @if ($errors->any())
-                                <input name="tags[]" type="checkbox" class="btn-check" id="tag-check-{{ $tag->id }}" autocomplete="off" value="{{ $tag->id }}" {{ in_array($tag->id, old('tags', [])) ? 'checked' : '' }}>
-                            @else
-                                <input name="tags[]" type="checkbox" class="btn-check" id="tag-check-{{ $tag->id }}" autocomplete="off" value="{{ $tag->id }}" {{ $project->tags->contains($tag) ? 'checked' : '' }}>
-                            @endif
-                            <label class="btn btn-outline-primary mb-2 d-inline-block" for="tag-check-{{ $tag->id }}">{{ $tag->name }}</label>
-                        @endforeach
+                    <label for="bathroom_num" class="form-label">Numero di Bagni</label>
+                    <input type="number" class="form-control" id="bathroom_num" name="bathroom_num" value="{{ old('bathroom_num') }}" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="sq_mt" class="form-label">Metri Quadrati</label>
+                    <input type="number" class="form-control" id="sq_mt" name="sq_mt" value="{{ old('sq_mt') }}" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="address" class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}" required>
+                </div>
+{{--
+                <div class="mb-3">
+                    <label for="latitude" class="form-label">Latitudine</label>
+                    <input type="text" class="form-control" id="latitude" name="latitude" value="{{ old('latitude') }}" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="longitude" class="form-label">Longitudine</label>
+                    <input type="text" class="form-control" id="longitude" name="longitude" value="{{ old('longitude') }}" required>
+                </div> --}}
+
+                <div class="mb-3">
+                    <label for="images" class="form-label">Immagine</label>
+
+                    <input type="file" class="form-control" id="images" name="images" value="{{ old('images') }}">
+                </div>
+
+                <!-- Extra Services -->
+                    <div class="mb-3">
+                        <label for="extra_services" class="form-label">Servizi Extra</label>
+                        <div class="form-check">
+                            @foreach($services as $service)
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="extra_services[]" value="{{ $service->id }}" id="service-{{ $service->id }}">
+                                    <label class="form-check-label" for="service-{{ $service->id }}">
+                                        {{ $service->name }}
+                                    </label>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
-                    @error("tags")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+
+
                 <div class="mb-3">
-                    <label for="link">Project link:</label>
-                    <input type="url" name="link" id="link" class="form-control mb-2" value="{{ old('link', $project->link) }}">
-                    @error("link")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                    <label for="visibility" class="form-label">Visibilità</label>
+                    <select class="form-select" id="visibility" name="visibility">
+                        <option value="1" {{ old('visibility') == 1 ? 'selected' : '' }}>Visibile</option>
+                        <option value="0" {{ old('visibility') == 0 ? 'selected' : '' }}>Nascosto</option>
+                    </select>
                 </div>
+
                 <div class="mb-3">
-                    <label for="image">Image</label>
-                    <input type="file" name="image" id="image" class="form-control mb-2" value="{{ old('image', $project->image) }}">
-                    @error("image")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                    <input type="submit" class="btn btn-primary" value="Crea Appartamento">
+                    <input type="reset" class="btn btn-secondary" value="Reset">
                 </div>
-                <div class="mb-3">
-                    <label for="content">Content</label>
-                    <textarea name="content" id="content" cols="30" rows="10" class="form-control mb-2">{{ old('content', $project->content) }}</textarea>
-                    @error("content")
-                        <div class="alert alert-danger">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <input type="submit" value="@yield('page-title')" class="btn btn-primary btn-sm">
-                <input type="reset" value="Reset fields" class="btn btn-warning btn-sm">
 
             </form>
         </div>
     </div>
 </div>
+
 @endsection
