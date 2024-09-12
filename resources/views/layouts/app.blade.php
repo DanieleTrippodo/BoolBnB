bab79607b53c9048e81c592f00240d7459efe181
Samuele-Chioda <samuelechioda@gmail.com>
2024-09-12 12:28:59 +0200

    Unified navigation and applied user-specific view based on authentication status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,69 +23,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <!-- Nome del sito che punta alla home (guest.index) -->
-                <a class="navbar-brand" href="{{ route('guest.index') }}">
-                    {{ config('app.name', 'BoolBnB') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Lato sinistro della navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <!-- Mostra il link "I tuoi appartamenti" solo se l'utente è autenticato -->
-                        @auth
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.apartments.index') }}">I tuoi appartamenti</a>
-                            </li>
-                        @endauth
-                    </ul>
-
-                    <!-- Lato destro della navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Link di autenticazione -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <!-- Mostra l'email o nome utente -->
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name ?? Auth::user()->email}}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Esci') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('partials.header')
 
         <main class="py-4">
             @yield('content')
