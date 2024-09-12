2eba55068b0560e38eae4b21e646b4757eccb914
DanieleTrippodo <trippododaniele@gmail.com>
2024-09-12 18:35:40 +0200

    added new logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BoolBnB</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/Small_Logo.png') }}">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
