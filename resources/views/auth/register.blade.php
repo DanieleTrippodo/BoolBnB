0cea00cc8d0996ad9acd7245e4c4bfe50f48ade8
Samuele-Chioda <samuelechioda@gmail.com>
2024-09-13 19:58:09 +0200

    fix style form placeholders and align password field width

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,7 +50,7 @@
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email">
+                                        value="{{ old('email') }}" required autocomplete="email" placeholder="Campo obbligatorio">
                                     <span id="emailError" style="color: red; display: none;"></span>
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -68,7 +68,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
+                                        required autocomplete="new-password" placeholder="Campo obbligatorio">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -78,14 +78,14 @@
                                 </div>
                             </div>
 
-                            <div class="row m-3">
+                            <div class="row mb-3">
                                 <label for="password-confirm"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Conferma Password') }}<span
                                         style="color: red;">*</span></label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
+                                        name="password_confirmation" required autocomplete="new-password" placeholder="Campo obbligatorio">
                                     <span id="passwordMismatch" style="color: red; display: none;">Le password non
                                         coincidono.</span>
                                 </div>
