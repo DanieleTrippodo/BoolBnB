23a66204fa9a14811ce453018c20a9cf756c2187
Samuele-Chioda <samuelechioda@gmail.com>
2024-09-19 12:09:14 +0200

    Aggiunto raggruppamento messaggi in base all'appartamento

--- resources/views/user/messages/index.blade.php
+++ resources/views/user/messages/index.blade.php
@@ -7,30 +7,32 @@
     @if ($messages->isEmpty())
         <p>Non hai ricevuto messaggi.</p>
     @else
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Appartamento</th>
-                        <th>Mittente</th>
-                        <th>Email del Mittente</th>
-                        <th>Messaggio</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($messages as $message)
+        @foreach ($messages->groupBy('apartment_id') as $apartmentId => $apartmentMessages)
+            <h2>{{ $apartmentMessages->first()->apartment->title }}</h2> <!-- Titolo dell'appartamento -->
+
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
                         <tr>
-                            <td>{{ $message->apartment->title }}</td>
-                            <td>{{ $message->name }}</td>
-                            <td>{{ $message->sender_email }}</td>
-                            <td>{{ $message->message }}</td>
-                            <td>{{ $message->created_at->format('d/m/Y H:i') }}</td>
+                            <th>Mittente</th>
+                            <th>Email del Mittente</th>
+                            <th>Messaggio</th>
+                            <th>Data</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        @foreach ($apartmentMessages as $message)
+                            <tr>
+                                <td>{{ $message->name }}</td>
+                                <td>{{ $message->sender_email }}</td>
+                                <td>{{ $message->message }}</td>
+                                <td>{{ $message->created_at->format('d/m/Y H:i') }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endforeach
     @endif
 </div>
 @endsection
